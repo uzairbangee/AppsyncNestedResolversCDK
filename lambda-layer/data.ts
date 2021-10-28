faaf0ac204abecda3a301e6f66029b5974e3aaab
murtazahuzaifa <murtaza.huzaifa2@gmail.com>
2021-10-28 16:54:55 +0500

    schema updated

--- lambda-layer/data.ts
+++ lambda-layer/data.ts
@@ -3,21 +3,25 @@ export let employees = [
         id: 1,
         name: 'John Smith',
         employerId: 1,
+        __typename: "Employee"
     },
     {
         id: 2,
         name: 'Lauren Armstrong',
         employerId: 1,
+        __typename: "Employee"
     },
     {
         id: 3,
         name: 'Henry Bautista',
         employerId: 1,
+        __typename: "Employee"
     },
     {
         id: 4,
         name: 'Jake Snarl',
         employerId: 2,
+        __typename: "Employee"
     },
 ];
 
@@ -25,9 +29,11 @@ export let employers = [
     {
         id: 1,
         name: 'Harrys pub',
+        __typename: "Employer"
     },
     {
         id: 2,
         name: 'UPS',
+        __typename: "Employer"
     },
 ];
